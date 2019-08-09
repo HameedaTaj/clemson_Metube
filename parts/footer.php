c1b987126014e26d4871bd54f29190aaa4953a9c
HameedaTaj <hameeda.dildar@gmail.com>
2019-08-09 15:44:03 -0400

    Add files via upload

---
+++ parts/footer.php
@@ -0,0 +1,25 @@
+<div id="footer" class = "navbar-fixed-bottom\">
+		<div class = "container" style="width:100%;">
+			<div class = "row" style="background-color:#522D80;">
+		        <div class="col-sm-4">
+		          <p style = "margin: 10px 0px; font-size:20px;">CLEMSON UNIVERSITY</p>
+                  <p style = "margin: -10px 0px; font-size:20px;">&#169; Copyright 2019</p>
+		        </div>
+		        <div class="col-sm-4">
+						<a href="index.php" class="pull-left"><img class = "img-responsive" src="css\logo.png" align="middle"></a>
+		        </div>
+		        <div class="col-sm-4" style="text-align: right;">
+                    <p style = "margin: 10px 0px; font-size:20px;"><a style = "color:#fff;" href = "contact-us.php">Contact Us</a></p>
+		        </div>
+	        </div>
+	    </div>
+</div>
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+<script>window.jQuery || document.write('<script src="js/vendor/jquery-1.11.2.min.js"><\/script>')</script>
+
+<script src="js/vendor/bootstrap.min.js"></script>
+
+<script src="js/main.js"></script>
+
+
+</div>
